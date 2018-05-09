f345d883c2b292774ffbe4b4daf94f8c7e8dff55
monts13 <38306542+monts13@users.noreply.github.com>
2018-05-09 11:18:41 -0400

    Add files via upload
    cadastro

---
+++ cadastro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Faça seu cadastro</title>
+	</head>
+	<body>
+		<form method="POST">
+			<input type="text" name="nome" id="nome" placeholder="Digite seu nome" class="form-control"><br>
+			<input type="password" name="senha" id="senha" placeholder="Digite sua senha" class="form-control"><br:
+			<input type="password" name="senha" id="senha" placeholder="repita sua senha" class="form-control"><br>
+			<input type="text" name="e-mail" id="e-email" placeholder="Digite seu e-mail" class="form-control"><br>
+
+			
+
+
+
+		</form>
+	</body>
+</html>
